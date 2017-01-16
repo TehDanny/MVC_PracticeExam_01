bd7f1f334282cdb6c343d7f60a9d1f3ca9b77680
Daniel Alexander Nedergaard <DanielxNedergaard@hotmail.dk>
2017-01-16 12:11:52 +0100

    Upstart

---
+++ MvcWebApplication/Models/BlogPostRepository.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MvcWebApplication.Models
+{
+    public class BlogPostRepository
+    {
+        public static List<BlogPost> blogPostList = new List<BlogPost>();
+    }
+}
